df3e14b05a70c7253ccb6376503336931c7cb632
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-22 19:27:46 +0100

    indent

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ var handlebars = require('handlebars');
 
 module.exports = function() {
 
-metalsmith('./')
-    .source('src/html/content')
-    .use(markdown())
-    .use(layouts({
-        engine: 'handlebars',
-        directory: 'src/html/layouts',
-        partials: 'src/html/layouts/partials'
-    }))
-    .destination('dist/site')
-    .use(watch({
-        paths: {
-            '${source}/**/*': '**/*',
-            'src/html/layouts/**/*': '**/*'
-        },
-        livereload: true
-    }))
-    .build(function(err) {
-      if (err) throw err;
-      console.log('Build finished!');
-    });
+    metalsmith('./')
+        .source('src/html/content')
+        .use(markdown())
+        .use(layouts({
+            engine: 'handlebars',
+            directory: 'src/html/layouts',
+            partials: 'src/html/layouts/partials'
+        }))
+        .destination('dist/site')
+        .use(watch({
+            paths: {
+                '${source}/**/*': '**/*',
+                'src/html/layouts/**/*': '**/*'
+            },
+            livereload: true
+        }))
+        .build(function(err) {
+          if (err) throw err;
+          console.log('Build finished!');
+        });
 }
