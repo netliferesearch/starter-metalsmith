3f182647f20b79edc4796754818ee616c9714dc0
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-25 20:16:54 +0100

    added config

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    src: {
+        content: 'src/html/content',
+        layout: 'src/html/layouts',
+        partials: 'src/html/layouts/partials'
+    },
+    watch: 'src/html/layouts/**/*',
+    dist: 'dist/site'
+};
--- index.js
+++ index.js
@@ -5,18 +5,18 @@ var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var watch = require('metalsmith-watch');
 var handlebars = require('handlebars');
+var config = require('./config');
 
 module.exports = function() {
-
     metalsmith('./')
-        .source('src/html/content')
+        .source(config.src.content)
         .use(markdown())
         .use(layouts({
             engine: 'handlebars',
-            directory: 'src/html/layouts',
-            partials: 'src/html/layouts/partials'
+            directory: config.src.layout,
+            partials: config.src.partials
         }))
-        .destination('dist/site')
+        .destination(config.dist)
         .use(watch({
             paths: {
                 '${source}/**/*': '**/*',
