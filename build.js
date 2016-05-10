5352d46cfe4ccfce58590bc1e21cf5709136c406
Fredrik Jensen <fredrik@netliferesearch.com>
2016-05-10 23:09:13 +0200

    Added build task

---
+++ build.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var metalsmith = require('metalsmith');
+var markdown = require('metalsmith-markdown');
+var layouts = require('metalsmith-layouts');
+var watch = require('metalsmith-watch');
+var handlebars = require('handlebars');
+var jade = require('jade');
+var twig = require('twig');
+
+var config = require('./config');
+
+module.exports = function(language) {
+    metalsmith('./')
+        .source(config.src.content)
+        .use(markdown())
+        .use(layouts({
+            engine: language,
+            directory: config.src.layout,
+            partials: config.src.partials
+        }))
+        .destination(config.dist)
+        .build(function(err) {
+          if (err) throw err;
+          console.log('Build finished!');
+        });
+}
--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
     run: require('./run'),
+    build: require('./build'),
     extendConfig: require('./extend-config')
 };
