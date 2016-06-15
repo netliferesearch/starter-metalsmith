93ff45464b96442c363f6d96d0df9e87a3a1614a
Jørgen Blindheim <jorgen.blindheim@gmail.com>
2016-06-15 13:51:58 +0200

    Handlebars templating language helpers

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
     src: {
-        content: 'src/html/content',
-        layout: 'src/html/layouts',
-        partials: 'src/html/layouts/partials'
+        content: './src/html/content',
+        layout: './src/html/layouts',
+        partials: './src/html/layouts/partials'
     },
-    watch: 'src/html/layouts/**/*',
-    dist: 'dist/site'
+    watch: './src/html/layouts/**/*',
+    dist: './dist/site'
 };
--- run.js
+++ run.js
@@ -6,14 +6,21 @@ var layouts = require('metalsmith-layouts');
 var inplace = require('metalsmith-in-place');
 var watch = require('metalsmith-watch');
 var handlebars = require('handlebars');
+var filenames = require('metalsmith-filenames');
+var handlebarsLayouts = require('handlebars-layouts');
 var jade = require('jade');
 var twig = require('twig');
 
 var config = require('./config');
 
 module.exports = function(language) {
+    if (language === 'handlebars') {
+        handlebars.registerHelper(handlebarsLayouts(handlebars));
+    }
+
     metalsmith('./')
         .source(config.src.content)
+        .use(filenames())
         .use(markdown())
         .use(layouts({
             engine: language,
@@ -22,7 +29,7 @@ module.exports = function(language) {
         }))
         .use(inplace({
             engine: language,
-            partials:  config.src.partials
+            partials: config.src.partials
         }))
         .destination(config.dist)
         .use(watch({
